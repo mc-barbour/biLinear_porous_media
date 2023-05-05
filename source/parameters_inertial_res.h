274b40420a322bef665b3f67b5d460e3207c752f
Michael Barbour <mbarbour89@gmail.com>
2023-05-05 10:20:35 -0700

    first commit

---
+++ source/parameters_inertial_res.h
@@ -0,0 +1,20 @@
+
+#ifndef aneurysm_vol_id
+#define aneurysm_vol_id 1 /* Identifier of aneurysm vol (domain) */ 
+#endif
+
+#ifndef aneurysm_surf_id
+#define aneurysm_surf_id 5 /* Identifier of aneurysm surf */ 
+#endif
+
+#ifndef dist_threshold
+#define dist_threshold 1e-3 /* Distance from aneurysm wall to core region*/ 
+#endif
+
+#ifndef core_inertial_res
+#define core_inertial_res 200 /* Value os permeability in core */ 
+#endif
+
+#ifndef wall_inertial_res
+#define wall_inertial_res 1 /* Value of permeability at wall */ 
+#endif
