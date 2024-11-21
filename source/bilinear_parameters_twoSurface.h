93902be45babdf57df9a93fa82bdb358d4d1e3c2
Michael Barbour <mbarbour89@gmail.com>
2024-11-21 09:43:57 -0800

    Adding codes for handling two input surfaces

---
+++ source/bilinear_parameters_twoSurface.h
@@ -0,0 +1,26 @@
+
+#ifndef aneurysm_vol_id
+#define aneurysm_vol_id 1 /* Identifier of aneurysm vol (domain) */ 
+#endif
+
+#ifndef aneurysm_surf_id
+#define aneurysm_surf_id 5 /* Identifier of aneurysm surf */ 
+#endif
+
+#ifndef neck_surf_id
+#define neck_surf_id 4 /* Identifier of aneurysm surf */ 
+#endif
+
+#ifndef dist_threshold
+#define dist_threshold 1.0e-3 /* Distance from aneurysm wall to core region*/ 
+#endif
+
+#ifndef core_porosity
+#define core_porosity .7217 /* Value os permeability in core */ 
+#endif
+
+#ifndef wall_porosity
+#define wall_porosity 1 /* Value of permeability at wall */ 
+#endif
+
+
